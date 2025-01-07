7a0071d34026d17d5683f1cc0b01ff9767e74d24
felipeclarindo <fglpc2@gmail.com>
2025-01-06 21:08:30 -0300

    refactor(base-endpoint): update endpoint to '/api' and include API description

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { DescriptionProps } from './types';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getDescription(): DescriptionProps {
+    return {
+      status: 'Api is running.',
+      name: 'Car Hub Api',
+      description: 'Api for car hub application.',
+      developer: {
+        name: 'felipeclarindo',
+        github: 'https://github.com/felipeclarindo',
+        linkedin: 'https://linkedin.com/in/felipeclarindo',
+      },
+    };
   }
 }
