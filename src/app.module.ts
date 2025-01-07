4a3e4eb37f0d9eaaf1ec5cbeb6584fcb87c43d2b
felipeclarindo <fglpc2@gmail.com>
2025-01-06 21:22:50 -0300

    feat(app-module): adding services and controllers created.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FuelsController } from './fuels/fuels.controller';
+import { FooterLinksController } from './footer-links/footer-links.controller';
+import { ManufacturersController } from './manufacturers/manufacturers.controller';
+import { YearsOfProductionController } from './years-of-production/years-of-production.controller';
+import { FooterLinksService } from './footer-links/footer-links.service';
+import { ManufacturersService } from './manufacturers/manufacturers.service';
+import { YearsOfProductionService } from './years-of-production/years-of-production.service';
+import { FuelsService } from './fuels/fuels.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    FuelsController,
+    FooterLinksController,
+    ManufacturersController,
+    YearsOfProductionController,
+  ],
+  providers: [
+    AppService,
+    FooterLinksService,
+    ManufacturersService,
+    YearsOfProductionService,
+    FuelsService,
+  ],
 })
 export class AppModule {}
